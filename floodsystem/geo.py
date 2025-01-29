93e4122fec89444439a70ca7de233e47532b03d8
Ethan Marshall <em2017@cam.ac.uk>
2025-01-29 15:57:38 +0000

    Implement stations within radius

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,15 @@ geographical data.
 
 """
 
+from haversine import haversine
 from .utils import sorted_by_key  # noqa
+
+
+def stations_within_radius(stations, centre, r):
+    """Returns a list of stations which have a radial distance of r km or lower from centre.
+    Centre is in the form (x, y)."""
+    found = []
+    for station in stations:
+        if haversine(centre, station.coord) <= r:
+            found.append(station)
+    return found
