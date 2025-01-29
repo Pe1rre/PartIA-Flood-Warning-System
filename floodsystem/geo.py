d45846a3772094ffb1b8218bea9f9dac98afd2d9
Peter Kelly <pk578@cam.ac.uk>
2025-01-29 12:32:40 +0000

    Implement stations by distance

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,16 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+
+from haversine import haversine, Unit
+from station import MonitoringStation
+def stations_by_distance(stations, p):
+    list_of_tuples = []
+    for station in stations:
+        distance_from_p = haversine(p, station.coord)
+        #making the tuple#
+        z = ( station.station_id, distance_from_p)
+        list_of_tuples.append(z)
+    list_of_tuples = sorted_by_key(list_of_tuples)
+    return list_of_tuples
