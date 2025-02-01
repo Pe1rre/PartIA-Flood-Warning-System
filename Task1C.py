ce6391c23dfc623f01b9cb9487074644c5d86e90
Ethan Marshall <em2017@cam.ac.uk>
2025-02-01 19:51:32 +0000

    Add demo program for Task1C

---
+++ Task1C.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2025 Ethan J. Marshall (em2017@cam.ac.uk)
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+
+def run():
+    """Requirements for Task 1C"""
+
+    # Build list of stations
+    stations = build_station_list()
+    # Get those within radius
+    matches = stations_within_radius(stations, (52.2053, 0.1218), 10)
+    matches.sort(key=lambda x: x.name)
+    for match in matches:
+        print(match.name)
+
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
