b88364d73854201bdb8266c719f3a955c50f940e
Ethan Marshall <em2017@cam.ac.uk>
2025-02-07 15:18:21 +0000

    Add demo program for Task 1D

---
+++ Task1D.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2025 Ethan J. Marshall (em2017@cam.ac.uk)
+# Copyright (C) 2025 Peter J. Kelly (pk578@cam.ac.uk)
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.utils import sorted_by_key
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_river
+
+
+def run():
+    """Requirements for Task 1D"""
+
+    # Build list of stations
+    stations = build_station_list()
+    rivers = stations_by_river(stations)
+
+    print("Number of rivers:", len(rivers))
+
+    rivers_array = rivers.items()
+    rivers_array = sorted_by_key(rivers_array, 0)
+    print("First 10:")
+    for i in range(0, 10):
+        print("\t", rivers_array[i][0])
+
+
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    run()
