4ad2928e51a6997f8ffb38443c26beca53a17f9d
Peter Kelly <pk578@cam.ac.uk>
2025-02-22 18:00:48 +0000

    Fixed sorting issue in flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -9,7 +9,7 @@ def stations_level_over_threshold(stations, tol):
             if relative_level > tol:
                 z = (station, relative_level)
                 list_of_tuples.append(z)
-    return sorted(list_of_tuples, key = z[1], reverse=True)
+    return sorted(list_of_tuples, key=lambda x: x[1], reverse=True)
 #Unit testing for this could just be to double check that the station levels are consistent OR that it is in the right order?
 
 #Task 2C
@@ -20,6 +20,6 @@ def stations_highest_rel_level(stations, N):
             relative_level = stations.relative_water_level(station)
             z = (station, relative_level)
             list_of_tuples.append(z)
-    list_of_tuples = sorted(list_of_tuples, key = z[1], reverse=False)
+    list_of_tuples = sorted(list_of_tuples, key=lambda x: x[1], reverse=False)
     #Use a slice to get the first N
     return (list_of_tuples[0:N+1])
