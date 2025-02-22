55a93455bbbb8f71aa30a6f18ef5f25f80731778
Ethan Marshall <em2017@cam.ac.uk>
2025-02-22 18:00:55 +0000

    Reformat and fixup issues in flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,6 +1,15 @@
+# Copyright (C) 2025 Peter. J. Kelly
+# Copyright (C) 2025 Ethan. J. Marshall
+#
+# SPDX-License-Identifier: MIT
+"""This module contains a collection of functions related to
+flood prediction and warning.
+
+"""
 
 
-#Task 2B
+# Unit testing for this could just be to double check that the station levels are consistent
+# OR that it is in the right order?
 def stations_level_over_threshold(stations, tol):
     list_of_tuples = []
     for station in stations:
@@ -10,9 +19,8 @@ def stations_level_over_threshold(stations, tol):
                 z = (station, relative_level)
                 list_of_tuples.append(z)
     return sorted(list_of_tuples, key=lambda x: x[1], reverse=True)
-#Unit testing for this could just be to double check that the station levels are consistent OR that it is in the right order?
 
-#Task 2C
+
 def stations_highest_rel_level(stations, N):
     list_of_tuples = []
     for station in stations:
@@ -21,5 +29,5 @@ def stations_highest_rel_level(stations, N):
             z = (station, relative_level)
             list_of_tuples.append(z)
     list_of_tuples = sorted(list_of_tuples, key=lambda x: x[1], reverse=False)
-    #Use a slice to get the first N
-    return (list_of_tuples[0:N+1])
+    # Use a slice to get the first N
+    return (list_of_tuples[0:N + 1])
