f1e3eeb478251443d9de85de5ff1f8ab846d351c
Ethan Marshall <em2017@cam.ac.uk>
2025-03-02 20:07:06 +0000

    Fix tasks 2B and C

--- Task2B.py
+++ Task2B.py
@@ -1,11 +1,14 @@
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
+
 
 def run():
     stations = build_station_list()
+    update_water_levels(stations)
     tol = 0.8
     tuples = stations_level_over_threshold(stations, tol)
-    return tuples
+    print(tuples)
+
 
 if __name__ == "__main__":
     print("*** Task 1F: CUED Part IA Flood Warning System ***")
--- Task2C.py
+++ Task2C.py
@@ -1,10 +1,14 @@
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
+
+
 def run():
     stations = build_station_list()
+    update_water_levels(stations)
     N = 10
     tuples = stations_highest_rel_level(stations, N)
-    return tuples
+    print(tuples)
+
 
 if __name__ == "__main__":
     print("*** Task 1F: CUED Part IA Flood Warning System ***")
