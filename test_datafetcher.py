35cc47ab9140c99206630eda9de252c34163cb17
Peter Kelly <pk578@cam.ac.uk>
2025-03-03 10:26:38 +0000

    Fix datafetcher test warning

--- test_datafetcher.py
+++ test_datafetcher.py
@@ -6,7 +6,7 @@
 import datetime
 
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.stationdata import build_station_list
 
 
 def test_build_station_list():
